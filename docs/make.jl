5b286efdcc34af969f6f15d4ee715af51aea8b96
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2018-10-27 20:10:52 +0200

    Try if doc is generated on osx

--- docs/make.jl
+++ docs/make.jl
@@ -8,11 +8,11 @@ makedocs(modules=[HermiteGF],
          doctest = false,
          format = :html,
          sitename = "HermiteGF.jl",
-         pages = ["Functions"    => "index.md"])
+         pages = ["Functions" => "index.md"])
 
 deploydocs(
     deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo   = "github.com/JuliaVlasov/HermiteGF.jl.git",
     julia  = "1.0",
-    osname = "linux"
+    osname = "osx"
  )
