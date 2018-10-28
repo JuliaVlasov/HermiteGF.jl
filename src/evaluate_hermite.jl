90c8217f8aaab2a76786e40a7877e1a748bf93ef
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2018-10-28 21:54:14 +0100

    Update src/evaluate_hermite.jl

--- src/evaluate_hermite.jl
+++ src/evaluate_hermite.jl
@@ -23,19 +23,19 @@ function evaluate_hermite(xk, N, epsilon, gamma)
 
   # Write the values of the first two Hermite functions to initialize the
   # three-term recurrence
-  result[:,1] .= exp.(-0.5.*((gamma*xk).^2))
-  result[:,2] .= gamma*xk.*sqrt(2).*exp.(-0.5.*((gamma*xk).^2))
+  result[:,1] = exp.(-0.5.*((gamma*xk).^2))
+  result[:,2] = gamma*xk.*sqrt(2).*exp.(-0.5.*((gamma*xk).^2))
 
   # Three term recurrence for the Hermite functions with argument gamma*x
   for i = 3:N
-    result[:,i] .= sqrt(2.0/(i-1)) .* (gamma*xk) .* result[:,i-1] .- sqrt((i-2)/(i-1)) .* result[:,i-2]
+    result[:,i] = sqrt(2.0/(i-1)) .* (gamma*xk) .* result[:,i-1] .- sqrt((i-2)/(i-1)) .* result[:,i-2]
   end
 
   # Scaling the Hermite functions with the exponential factor
   # exp(-epsilon^2 x^2 + (gamma x)^2/2)
   
   for i=1:N
-      result[:,i] .= (pi^(1/4)) * result[:,i] .* exp.((xk.^2) .* (gamma*gamma*0.5 - epsilon^2))
+      result[:,i] = (pi^(1/4)) * result[:,i] .* exp.((xk.^2) .* (gamma*gamma*0.5 - epsilon^2))
   end
 
   result
